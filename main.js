beadec3fc669996408243e83d44b31546eef0cf3
missderpette <mderpette@gmail.com>
2023-06-09 00:06:15 +0800

    insurance

--- main.js
+++ main.js
@@ -200,6 +200,7 @@ function handleClick() {
         .then(response => response.json())
         .then(data => {
 
+            alert(data)
             const allOutput = document.querySelector('#all-output');
         
             data.forEach(product => {
